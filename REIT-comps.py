4da486905f2fbfd34f0b19cc27d65f525cb3f381
nehat312 <nate.ehat@gmail.com>
2022-08-21 15:03:18 -0400

    Update REIT-comps.py

--- REIT-comps.py
+++ REIT-comps.py
@@ -645,9 +645,11 @@ def display_sector_charts(sector_input):
 tab_1, tab_2, tab_3, tab_4, tab_5, tab_6, tab_7, tab_8, tab_9, tab_10 = st.tabs(['APARTMENT', 'OFFICE', 'HOTEL', 'MALL', 'STRIP CENTER', 'NET LEASE', 'INDUSTRIAL', 'SELF-STORAGE', 'DATA CENTER', 'HEALTHCARE'])
 with tab_1:
     st.header('APARTMENT')
-    x = apartment_reits_close.index
+    apt_x = apartment_reits_close.index
     # y = apartment_reits_close[ticker_input]
-    st.plotly_chart(px.line(apartment_reits_close,
+    for i in apartment_reits_close.columns:
+        st.plotly_chart(px.line(x=apt_x,
+                                y=apartment_reits_close[i],
                             # x,
                             # y,
             # apartment_reits_close[{ticker_input}],
@@ -661,8 +663,8 @@ with tab_1:
             # hover_data=ticker_output_df[['sector', 'reportPeriod']],
             # title=f'{ticker_input} SHARE PRICE',
             labels=chart_labels,
-            height=1000,
-            width=1000,
+            height=800,
+            width=800,
             ))
 
     with st.form('APARTMENT TICKER METRICS'):
